74ddcbe172994ecbf07945ebadadd9b391b8bb25
Emerson Marques Pedro <emersonmp@gmail.com>
2024-03-09 16:57:56 -0300

    Novo init adicionado

---
+++ Retangulo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using System;
+
+namespace CalculoRetangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double altura, largura;
+
+            Console.Write("Digite a altura do retângulo: ");
+            altura = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a largura do retângulo: ");
+            largura = double.Parse(Console.ReadLine());
+
+            Retangulo retangulo = new Retangulo(altura, largura);
+
+            Console.WriteLine("\nÁrea: " + retangulo.CalcularArea().ToString("F2"));
+            Console.WriteLine("Perímetro: " + retangulo.CalcularPerimetro().ToString("F2"));
+            Console.WriteLine("Diagonal: " + retangulo.CalcularDiagonal().ToString("F2"));
+        }
+    }
+}
