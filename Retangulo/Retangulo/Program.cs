65fbecdc7dd4cad84e521ab2c9b490a86cabb496
Emerson Marques Pedro <emersonmp@gmail.com>
2024-03-08 22:06:59 -0300

    Primeiro arquivo criado sem classe

---
+++ Retangulo/Retangulo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RetanguloCalculo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double largura, altura, area, perimetro, diagonal;
+
+            Console.Write("Digite a largura do retângulo: ");
+            largura = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a altura do retângulo: ");
+            altura = double.Parse(Console.ReadLine());
+
+            area = largura * altura;
+            perimetro = 2 * (largura + altura);
+            diagonal = Math.Sqrt(Math.Pow(largura, 2) + Math.Pow(altura, 2));
+
+            Console.WriteLine("\nÁrea = " + area.ToString("F2"));
+            Console.WriteLine("Perímetro = " + perimetro.ToString("F2"));
+            Console.WriteLine("Diagonal = " + diagonal.ToString("F2"));
+
+          
+        }
+    }
+}
